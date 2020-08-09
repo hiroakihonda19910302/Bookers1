72e19139bd5808f0272c5a4cf90d7e864f687431
hiroakihonda19910302 <hiroaki.honda3918@gmail.com>
2020-08-10 02:23:32 +0900

    課題提出２

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,14 +3,35 @@ class BooksController < ApplicationController
   end
 
   def index
+  	@books = Book.all
+  	@book = Book.new
   end
 
   def show
+  	@book = Book.find(params[:id])
   end
 
   def new
   end
 
+  def create
+  	book = Book.new(book_params)
+    book.save
+    redirect_to book_path
+  end
+
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
   end
 end
